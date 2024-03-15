27f9b32da6acc7dc4d6ea7a99a38dc10cb7bffd7
Nicole Olave <nicoleconsuelo@Nicoles-MacBook-Pro.local>
2024-03-15 16:13:52 +0100

    pruebas personales renderItems

--- src/view.js
+++ src/view.js
@@ -1,6 +1,21 @@
 export const renderItems = (data) => {
-  console.log(data)
+  console.log(data);
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
-};
 
+  const body = document.body;
+  const ul = document.createElement("ul");
+  const h1 = document.createElement("h1"); // <h1></h1>
+  const texto = document.createTextNode("Hola Bella");
+  h1.appendChild(texto); // al h1 le agrego un hijo texto  // <h1>Hola Bella</h1>
+
+  body.appendChild(h1);
+
+  body.appendChild(ul);
+
+  data.forEach((element) => {
+    console.log(element.extraInfo.abilities); 
+  });
+
+  console.log(body);
+  return ul;
+};
