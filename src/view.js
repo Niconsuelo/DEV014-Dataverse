c3b696dd24a16e0318f5a53d5f526d014e713c47
Milen <millalen.pg@gmail.com>
2024-03-20 06:03:10 -0300

    Style features.Also, changes in index.html and view.js

--- src/view.js
+++ src/view.js
@@ -3,20 +3,19 @@ export const renderItems = (data) => {
   data.forEach((character) => {
     const li = `
       <li itemscope itemtype="Personaje" id=${character.id}>
-      <div class="card">
-        <div class="card-container "> 
-        <img src=${character.imageUrl} alt=${character.name} class=" card img">
-        
-
-        <div class="card-text">
-        <h3 itemprop="name" class="card-tittle">${character.name}</h3>
-          <div itemprop="nenType"><strong>Tipo de Nen:</strong>${character.facts.nenType}</div>
-          <div itemprop="ocupation"><strong>Ocupación:</strong>${character.facts.ocupation}</div>
-          <div itemprop="abilities"><strong>Habilidades:</strong>${character.extraInfo.abilities}</div>
+        <div class="card">
+          <div class="imgs-container"> 
+            <img src=${character.imageUrl} alt=${character.name} class="card-img">
+            <img src="https://d100mj7v0l85u5.cloudfront.net/s3fs-public/2022-10/futuro-codigo-de-barras.png" alt="barcode" class="barcode-img">
+          </div> 
+          <div class="card-text">
+            <h3 itemprop="name" class="card-tittle">${character.name}</h3>
+            <p1 itemprop="nenType"><strong>Tipo de Nen:</strong>${character.facts.nenType}</p1>
+            <p2 itemprop="ocupation"><strong>Ocupación:</strong>${character.facts.ocupation}</p2>
+            <p3 itemprop="abilities"><strong>Habilidades:</strong>${character.extraInfo.abilities}</p3>
+          </div>
         </div>
-        </div>
-      </div>
-    </li>
+      </li>
     `;
     ul.innerHTML = ul.innerHTML + li; // Inserta cada tarjeta en el contenedor
   });
