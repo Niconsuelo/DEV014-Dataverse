2c308953afb8a0a43ea7498e41251ffe077e1154
Nicole Olave <nicoleconsuelo@Nicoles-MacBook-Pro.local>
2024-03-26 17:07:39 +0100

    se realiza sortby asc y desc, se realiza filtro nenType, se cambian nombre de parametro y funcion segun el readme, se elimina h2 y se agrega un hijo a main, un div que contiene el root, en html en el filternenType se cambian los valores de value, los nombres de value deben ser identicos a los elementos de los objetos

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,17 +1,25 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 //filtrar conjunto de datos
-export function filterOcupation(data, filterby, value) {
+export function filterData(data, filterby, value) {
   //arreglo de objetos, propiedad por la cual se desea filtrar, valor especifico dentro de la propiedad
   const resultOcupation = data.filter(
     //filter->toma parametros individual de data(character)
     //compara estrictamente caracterfactfilterby
     (character) => character.facts[filterby] === value
-
   );
   return resultOcupation;
 }
 
+export function sortData(data, sortby, value) {
+  if (value === "asc") {
+    return data.sort((a, b) => a[sortby].localeCompare(b[sortby]));
+  }
+  if (value === "desc") {
+    return data.sort((a, b) => b[sortby].localeCompare(a[sortby]));
+  }
+}
 
 export const anotherExample = () => {
   return [];
 };
+
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { filterOcupation } from "./dataFunctions.js";
+import { sortData } from "./dataFunctions.js";
+import { filterData } from "./dataFunctions.js";
 import { renderItems } from "./view.js";
 import data from "./data/dataset.js";
 
@@ -11,28 +12,47 @@ searchRoot.appendChild(renderItems(data));
 const selectfilterOcupation = document.getElementById("filter-ocupation");
 
 // escuchador de eventos al valor, cuando este cambie segun la option del usuarix
-selectfilterOcupation.addEventListener("change", function () {
-  // se debe tener el valor actual del elemento que se selecciono (option) this:obtiene el valor de la opcion seleccionada
-  const optionFilter = this.value;
+selectfilterOcupation.addEventListener("change", function (event) {
+  // se debe tener el valor actual del elemento que se selecciono (option)
+  const optionFilter = event.target.value;
   //option filter, retornara el resultado del criterio segun la opcion que eligio el usuarix
 
-  const resultSelection = filterOcupation(data, "ocupation", optionFilter);
+  const resultSelection = filterData(data, "ocupation", optionFilter);
+
   //debemos eliminar elementos uno por uno de la interfaz
-  const ulList = document.querySelectorAll("ul");
+  const root = document.querySelector("#root");
   // contiene todos los ul
-  ulList.forEach((lista) => {
-    //eliminar elementos del listado de ul
-    lista.remove();
-  });
+  root.innerHTML = "";
+
   searchRoot.appendChild(renderItems(resultSelection));
 });
 
-const orderData = data;
-//sort permite ordenar pero no cambia
-//
-const ascName = orderData.sort((a, b) => a.name.localeCompare(b.name));
+const selectfilterNentype = document.getElementById("filter-nen");
 
-console.log("Orden Ascendiente:", ascName);
+selectfilterNentype.addEventListener("change", function (event) {
+  const filterNentype = event.target.value;
+  const resultoptionNentype = filterData(data, "nenType", filterNentype);
+
+  const root = document.querySelector("#root");
+
+  root.innerHTML = "";
+
+  searchRoot.appendChild(renderItems(resultoptionNentype));
+});
+
+const orderCharacter = document.getElementById("sort-by");
+
+orderCharacter.addEventListener("change", function (event) {
+  const sortOrder = event.target.value;
+
+  const resultORder = sortData(data, "name", sortOrder);
+
+  const root = document.querySelector("#root");
+
+  root.innerHTML = "";
+
+  searchRoot.appendChild(renderItems(resultORder));
+});
 
 //console.log(example, renderItems(data), data);
 
