95cb0ab4ebb30ddb3ec4797f6a0c187bc488e97d
Nicole Olave <nicoleconsuelo@Nicoles-MacBook-Pro.local>
2024-03-26 14:07:04 +0100

    se realizan cambios a datafunction y main, con respecto a los parametros, se agredó el parametro filterby a la function filterOcupation para hacer la funcion dinamica

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,8 +1,8 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
-export function filterOcupation(data, value) {
+export function filterOcupation(data, filterby, value) {
   const resultOcupation = data.filter(
-    (character) => character.facts.ocupation === value
+    (character) => character.facts[filterby] === value
   );
 
   return resultOcupation;
